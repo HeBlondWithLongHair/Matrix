50278c6ff173c3a7c5e5e72b4121a04f28045982
HeBlondWithLongHair <advol231231231@gmail.com>
2023-03-13 00:28:40 +0300

    Добавьте файлы проекта.

---
+++ Matrix.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+public class Matrix
+{
+
+    public static void sumMatrix(double[,] firstArray, double[,] secondArray)
+    {
+        double[,] sumArray = new double[firstArray.GetLength(0), firstArray.GetLength(1)];
+
+        if (firstArray.GetLength(0) == secondArray.GetLength(0))
+        {
+            if (firstArray.GetLength(1) == secondArray.GetLength(1))
+            {
+                for (int i = 0; i < firstArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < firstArray.GetLength(1); j++)
+                    {
+                        sumArray[i, j] = firstArray[i, j] + secondArray[i, j];
+                        Console.Write(sumArray[i, j] + " ");
+                    }
+                    Console.WriteLine();
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Столбцы матриц должны быть одного размера");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Строки матриц должны быть одного размера!");
+            return;
+        }
+        
+        
+    }
+
+    public static void multMatrixByNumber(double[,] firstArray, double number)
+    {
+        
+
+        for (int i = 0; i < firstArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < firstArray.GetLength(0); j++)
+            {
+                firstArray[i, j] = firstArray[i, j] * number;
+                Console.Write(firstArray[i,j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+        
+    }
+
+    public static void print(double[,] firstArray)
+    {
+        for (int i = 0; i < firstArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < firstArray.GetLength(1); j++)
+            {
+                Console.Write(firstArray[i,j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    public static void multMatrix(double[,] firstArray, double[,] secondArray)
+    {
+        double[,] Array = new double[firstArray.GetLength(0), firstArray.GetLength(1)];
+
+        if (firstArray.GetLength(0) == secondArray.GetLength(0))
+        {
+            if (firstArray.GetLength(1) == secondArray.GetLength(1))
+            {
+                for (int i = 0; i < firstArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < firstArray.GetLength(1); j++)
+                    {
+                        Array[i, j] = firstArray[i, j] * secondArray[i, j];
+                        Console.Write(Array[i, j] + " ");
+                    }
+                    Console.WriteLine();
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Столбцы матриц должны быть одного размера");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Строки матриц должны быть одного размера!");
+            return;
+        }
+
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[,] firstArray =  { {1,2,3,4,5 },{5,4,3,2,1 },{0,9,8,7,6 },{6,7,8,9,0 } };
+        double[,] secondArray =  { { 1,2,3,4,5 },{ 5,4,3,2,1 },{ 0,9,8,7,6 },{ 6,7,8,9,0 } };
+
+        Console.WriteLine("Сложение одной матрицы с другой: ");
+        Matrix.sumMatrix(firstArray, secondArray);
+
+        Console.WriteLine("\nУмножение матрицы на число:");
+        Matrix.multMatrixByNumber(firstArray, 5);
+
+        Console.WriteLine("\nВывод матрицы: ");
+        Matrix.print(firstArray);
+
+        Console.WriteLine("\nУмножение матриц: ");
+        Matrix.multMatrix(firstArray, secondArray);
+
+
+    }
+}
+
